8d8591e55fd46c28fc69c7cab47f3ecbe7df47f6
Marcin Konieczny <marcin_k@coni.pl>
2017-10-08 21:37:27 +0200

    Version 2.0

--- src/Direction.java
+++ src/Direction.java
@@ -1,8 +1,9 @@
 public enum Direction {
-    NORTH("Pólnoc", 1),
-    SOUTH("Południe", 2),
-    WEST("Zachód", 3),
-    EAST("Wschód", 4);
+    NORTH("Pólnoc", 0),
+    SOUTH("Południe", 1),
+    WEST("Zachód", 2),
+    EAST("Wschód", 3),
+    UNDEFINED("Niezdefiniowany", 5);
 
     private final String description;
     private final int number;
@@ -19,5 +20,23 @@ public enum Direction {
         this.description = description;
         this.number = number;
 
+    }
+
+    public void convert(int number) {
+        if (number == NORTH.getNumber()) {
+            System.out.println(NORTH.getDescription());
+        }
+        if (number == EAST.getNumber()) {
+            System.out.println(EAST.getDescription());
+        }
+        if (number == SOUTH.getNumber()) {
+            System.out.println(SOUTH.getDescription());
+        }
+        if (number == WEST.getNumber()) {
+            System.out.println(WEST.getDescription());
+        }
+        if (number < 0 || number > 4) {
+            System.out.println(Direction.UNDEFINED.getDescription());
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,19 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        Direction direction;
+        Direction direction = Direction.NORTH;
         System.out.println("Podaj kierunek: ");
+
         try {
             direction = Direction.valueOf(scanner.nextLine());
             ShowDirection.showDirection(direction);
         } catch (IllegalArgumentException e) {
             System.out.println("Uruchom program ponownie i napisz jeszcze raz");
         }
+
+
+
+        System.out.println("Podaj numer kierunku: ");
+        direction.convert(scanner.nextInt());
     }
 }
